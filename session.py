190afc0b3cd11be5bad2ac603260e63c4e2cfcf7
Vasily E <laer.18@gmail.com>
2019-01-08 18:45:38 +0300

    Session: simple client identification and SMS receiving
    Signed-off-by: Vasily E <laer.18@gmail.com>

--- session.py
+++ session.py
@@ -1,7 +1,16 @@
+from sms import (
+    SMS
+)
+from client import (
+    Client
+)
+
 class Session(object):
 
     def __init__(self, sock):
         self._s = sock;
+        self._clients = {}
+        # self._current_client = None # it's better to raise AttributeError
 
     def send(self, p):
         print("<< " + str(p))
@@ -9,25 +18,46 @@ class Session(object):
             sent = self._s.send(p)
             p=p[sent:]
 
+    def sendnl(self, l):
+        self.send(l + b"\n")
+
     def recv(self):
         res = b""
         while True:
             p = self._s.recv(1024)
             if not p:
                 break
+            print(">> " + str(p))
             res += p
             if p[-1] == ord("\n"):
                 break
-        print(">> " + str(p))
-        return p
+        return res
 
     def run(self):
-        p = self.recv()
+        while True:
+            p = self.recv()
 
-        if not p:
-            return
+            if not p:
+                return
 
-        for b in p:
+            b = p[0]
             if b == ord("G"):
                 self.send(b"g\n")
                 return
+            elif b == ord("S"):
+                sms = SMS(p[1:-1])
+                self._current_client.add_sms(sms)
+                self.sendnl(b"s")
+                # print("%s" % sms)
+            elif b == ord("I"):
+                _id = p[1:-1].decode("utf-8")
+                self.sendnl(b"i")
+                try:
+                    client = self._clients[_id]
+                except KeyError:
+                    client = Client(_id)
+                    self._clients[_id] = client
+                self._current_client = client
+            else:
+                self.sendnl(b"eUnknown packet code %c" % b)
+
