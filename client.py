ca67c5680156cc157ccc41f15e22ea80d907947e
Vasily E <laer.18@gmail.com>
2019-01-08 22:59:54 +0300

    Client, SMS: setialization to Python
    Signed-off-by: Vasily E <laer.18@gmail.com>

--- client.py
+++ client.py
@@ -1,8 +1,26 @@
+from common import (
+    make_python_identifier
+)
+
+
 class Client(object):
 
     def __init__(self, _id):
         self._id = _id
         self._all_sms = []
 
-    def add_sms(self, sms):
-        self._all_sms.append(sms)
+    def add_sms(self, *sms):
+        for s in sms:
+            self._all_sms.append(s)
+
+    def __dfs_children__(self):
+        return self._all_sms
+
+    def __var_base__(self):
+        return make_python_identifier(self._id)[0]
+
+    def __gen_code__(self, g):
+        g.gen_code(self)
+        g.write(g.nameof(self) + ".add_sms(*")
+        g.pprint(self._all_sms)
+        g.line(")")
--- sms.py
+++ sms.py
@@ -1,6 +1,12 @@
 class SMS(object):
-    def __init__(self, raw):
-        self._raw = raw;
+    def __init__(self, _raw):
+        self._raw = _raw;
 
     def __str__(self):
         return str(self._raw)
+
+    def __var_base__(self):
+        return "sms"
+
+    def __gen_code__(self, g):
+        g.gen_code(self)
