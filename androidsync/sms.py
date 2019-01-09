9c1d84e831e552ce0f4c2cc81746ba13c0c0e616
Vasily E <laer.18@gmail.com>
2019-01-09 20:00:08 +0300

    SMS: few raw data interpretation helpers
    Signed-off-by: Vasily E <laer.18@gmail.com>

--- androidsync/sms.py
+++ androidsync/sms.py
@@ -2,11 +2,39 @@ __all__ = [
     "SMS"
 ]
 
+from json import (
+    loads
+)
+from common import (
+    lazy
+)
+
 
 class SMS(object):
 
     def __init__(self, _raw):
         self._raw = _raw;
+
+    @lazy
+    def message(self):
+        res = self["body"].encode("charmap").decode("utf-8")
+        return res
+
+    @lazy
+    def line(self):
+        res = self.message.replace("\n", " ")
+        return res
+
+    @lazy
+    def json(self):
+        return loads(self._raw)
+
+    @lazy
+    def headings(self):
+        return tuple(self.json.keys())
+
+    def __getitem__(self, name):
+        return self.json[name]
 
     def __str__(self):
         return str(self._raw)
