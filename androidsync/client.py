1b6d8f04b1270a75cb69d756e95763f344862230
Vasily E <laer.18@gmail.com>
2019-01-09 13:50:01 +0300

    Client: SMS duplication prevention based on dict
    Signed-off-by: Vasily E <laer.18@gmail.com>

--- androidsync/client.py
+++ androidsync/client.py
@@ -11,14 +11,15 @@ class Client(object):
 
     def __init__(self, _id):
         self._id = _id
-        self._all_sms = []
+        self._all_sms = {}
 
     def add_sms(self, *sms):
         for s in sms:
-            self._all_sms.append(s)
+            # prevent duplication using raw SMS as hash
+            self._all_sms[hash(s._raw)] = s
 
     def __dfs_children__(self):
-        return self._all_sms
+        return tuple(self._all_sms.values())
 
     def __var_base__(self):
         return make_python_identifier(self._id)[0]
@@ -26,5 +27,5 @@ class Client(object):
     def __gen_code__(self, g):
         g.gen_code(self)
         g.write(g.nameof(self) + ".add_sms(*")
-        g.pprint(self._all_sms)
+        g.pprint(tuple(self._all_sms.values()))
         g.line(")")
