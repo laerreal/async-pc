5e1e35cc771cdcc4e0fd66ae09f427817e42648c
Vasily E <laer.18@gmail.com>
2019-01-09 20:09:03 +0300

    ClientView: simple SMS view
    Signed-off-by: Vasily E <laer.18@gmail.com>

--- androidsync/widgets/client_view.py
+++ androidsync/widgets/client_view.py
@@ -6,7 +6,11 @@ from six.moves.tkinter import (
     Frame
 )
 from six.moves.tkinter.ttk import (
+    Treeview,
     Notebook
+)
+from .scrollbars import (
+    add_scrollbars
 )
 
 
@@ -26,5 +30,39 @@ class ClientView(Frame):
         self._sms_w = sms_w = Frame(nb)
         nb.add(sms_w, text = "SMS")
 
+        sms_w.rowconfigure(0, weight = 1)
+        sms_w.columnconfigure(0, weight = 1)
+
+        self._sms_tv = sms_tv = Treeview(sms_w, columns = ("address", "body"))
+        sms_tv.heading("#0", text = "#")
+        sms_tv.heading("address", text = "Sender")
+        sms_tv.heading("body", text = "Message")
+        sms_tv.column("#0", minwidth = 50)
+        sms_tv.column("address", minwidth = 100)
+        sms_tv.column("body", minwidth = 500)
+
+        sms_tv.grid(row = 0, column = 0, sticky = "NESW")
+
+        add_scrollbars(sms_w, sms_tv)
+
         self._calls_w = calls_w = Frame(nb)
         nb.add(calls_w, text = "Calls")
+
+        self._update()
+
+    def _update(self):
+        sms_tv = self._sms_tv
+
+        # XXX: a dark side of power
+        sms_tv.delete(*sms_tv.get_children())
+
+        all_sms = list(self._client._all_sms.items())
+
+        s_all_sms = sorted(all_sms, key = lambda h_sms : int(h_sms[1]["date"]))
+
+        for i, (_hash, sms) in enumerate(s_all_sms):
+            sms_tv.insert("", "end",
+                text = str(i),
+                values = (sms["address"], sms.line),
+                tags = (_hash,)
+            )
