24bf0f1a18a64dd811cb95683dce738c65b0d306
Vasily E <laer.18@gmail.com>
2019-01-12 20:54:03 +0300

    Client: support contacts related items
    Signed-off-by: Vasily E <laer.18@gmail.com>

--- androidsync/client.py
+++ androidsync/client.py
@@ -54,20 +54,50 @@ class Client(object):
         add_raw_unique(self._all_calls, *calls)
         return self
 
+    def add_contacts(self, *c):
+        add_raw_unique(self._contacts, *c)
+        return self
+
+    def add_raw_contacts(self, *c):
+        add_raw_unique(self._raw_contacts, *c)
+        return self
+
+    def add_contacts_data(self, *c):
+        add_raw_unique(self._contacts_data, *c)
+        return self
+
+    def add_mime_types(self, *c):
+        add_raw_unique(self._mime_types, *c)
+        return self
+
     def __dfs_children__(self):
-        return tuple(chain(self._all_sms.values(), self._all_calls.values()))
+        return tuple(chain(
+            self._all_sms.values(),
+            self._all_calls.values(),
+            self._contacts.values(),
+            self._raw_contacts.values(),
+            self._contacts_data.values(),
+            self._mime_types.values()
+        ))
 
     def __var_base__(self):
         return make_python_identifier(self._id)[0]
 
     def __gen_code__(self, g):
         g.gen_code(self)
-        g.write(g.nameof(self) + ".add_sms(*")
-        g.pprint(tuple(self._all_sms.values()))
-        g.line(")")
-        g.write(g.nameof(self) + ".add_calls(*")
-        g.pprint(tuple(self._all_calls.values()))
-        g.line(")")
+        for method, container in (
+            ("add_sms", self._all_sms),
+            ("add_calls", self._all_calls),
+            ("add_contacts", self._contacts),
+            ("add_raw_contacts", self._raw_contacts),
+            ("add_contacts_data", self._contacts_data),
+            ("add_mime_types", self._mime_types),
+        ):
+            if not container:
+                continue
+            g.write(g.nameof(self) + "." + method + "(*")
+            g.pprint(tuple(container.values()))
+            g.line(")")
 
     def get_conversations(self):
         addresses = defaultdict(list)
